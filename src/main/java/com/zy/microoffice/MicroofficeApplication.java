cf20dddbbf82795dffe627cedb06fcfe46f4fe50
hahajing <444511958@qq.com>
2021-01-05 17:58:44 +0800

    支持war包部署

--- src/main/java/com/zy/microoffice/MicroofficeApplication.java
+++ src/main/java/com/zy/microoffice/MicroofficeApplication.java
@@ -4,13 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.zy.microoffice.mapper")
 @SpringBootApplication()
-public class MicroofficeApplication {
+public class MicroofficeApplication  extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(MicroofficeApplication.class, args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MicroofficeApplication.class);
+    }
 }
